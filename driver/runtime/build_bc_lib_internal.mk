05e2a338b2b66f30d86687c3de4e48d30a2db8d4
Your Name <you@example.com>
2015-07-12 16:54:01 +0200

    Use Qualcomm Snapdragon LLVM Compiler 3.5 to compile driver runtime

--- driver/runtime/build_bc_lib_internal.mk
+++ driver/runtime/build_bc_lib_internal.mk
@@ -18,6 +18,43 @@ ifndef BCC_RS_TRIPLE
 BCC_RS_TRIPLE := $($(LOCAL_2ND_ARCH_VAR_PREFIX)RS_TRIPLE)
 endif
 
+bc_clang_cc1_cflags :=
+bc_cflags_opt :=
+ifeq ($(USE_CLANG_QCOM),true)
+LLVM_PREBUILTS_PATH_QCOM_3.5 := prebuilts/clang/linux-x86/host/llvm-Snapdragon_LLVM_for_Android_3.5/prebuilt/linux-x86_64/bin
+LLVM_PREBUILTS_HEADER_PATH_QCOM_3.5 := $(LLVM_PREBUILTS_PATH_QCOM)/../lib/clang/3.5.0/include/
+
+LOCAL_CLANG_EXECUTABLE := $(LLVM_PREBUILTS_PATH_QCOM_3.5)/clang 
+LOCAL_LLVM_AS_EXECUTABLE := $(LLVM_PREBUILTS_PATH_QCOM_3.5)/llvm-as
+LOCAL_LLVM_LINK_EXECUTABLE := $(LLVM_PREBUILTS_PATH_QCOM_3.5)/llvm-link
+
+bc_cflags_opt := \
+  -Ofast -fno-fast-math -mcpu=krait2 -mfpu=neon -mfloat-abi=softfp -marm -v \
+  -muse-optlibc \
+  -fvectorize-loops \
+  -fomit-frame-pointer \
+  -foptimize-sibling-calls \
+  -ffinite-math-only \
+  -funsafe-math-optimizations \
+  -funroll-loops \
+  -fstrict-aliasing \
+  -fstack-protector \
+  $(CLANG_QCOM_CONFIG_KRAIT_ALIGN_FLAGS) \
+  $(CLANG_QCOM_CONFIG_KRAIT_MEM_FLAGS) \
+  $(CLANG_QCOM_CONFIG_KRAIT_PARALLEL_FLAGS) \
+  -fstrict-aliasing \
+  -Wstrict-aliasing=2 \
+  -Werror=strict-aliasing \
+  -mllvm -aggressive-jt
+
+bc_clang_cc1_cflags += -target-feature +vfp4 -target-feature +neon
+else
+bc_cflags_opt := #$(arch_variant_cflags)
+LOCAL_CLANG_EXECUTABLE := $(LLVM_PREBUILTS_PATH)/clang$(BUILD_EXECUTABLE_SUFFIX)
+LOCAL_LLVM_AS_EXECUTABLE := $(LLVM_PREBUILTS_PATH)/llvm-as$(BUILD_EXECUTABLE_SUFFIX)
+LOCAL_LLVM_LINK_EXECUTABLE := $(LLVM_PREBUILTS_PATH)/llvm-link$(BUILD_EXECUTABLE_SUFFIX)
+endif
+
 # Set these values always by default
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
@@ -26,11 +63,12 @@ include $(BUILD_SYSTEM)/base_rules.mk
 
 BCC_STRIP_ATTR := $(BUILD_OUT_EXECUTABLES)/bcc_strip_attr$(BUILD_EXECUTABLE_SUFFIX)
 
-bc_clang_cc1_cflags :=
+
 ifeq ($(BCC_RS_TRIPLE),armv7-none-linux-gnueabi)
 # We need to pass the +long64 flag to the underlying version of Clang, since
 # we are generating a library for use with Renderscript (64-bit long type,
 # not 32-bit).
+#BCC_RS_TRIPLE := armv7a-none-linux-gnueabi
 bc_clang_cc1_cflags += -target-feature +long64
 endif
 bc_translated_clang_cc1_cflags := $(addprefix -Xclang , $(bc_clang_cc1_cflags))
@@ -38,8 +76,8 @@ bc_translated_clang_cc1_cflags := $(addprefix -Xclang , $(bc_clang_cc1_cflags))
 bc_cflags := -MD \
              $(RS_VERSION_DEFINE) \
              -std=c99 \
-             -c \
              -O3 \
+             -c \
              -fno-builtin \
              -emit-llvm \
              -target $(BCC_RS_TRIPLE) \
@@ -47,7 +85,9 @@ bc_cflags := -MD \
              $($(LOCAL_2ND_ARCH_VAR_PREFIX)RS_TRIPLE_CFLAGS) \
              $(LOCAL_CFLAGS) \
              $(bc_translated_clang_cc1_cflags) \
-             $(LOCAL_CFLAGS_$(my_32_64_bit_suffix))
+             $(LOCAL_CFLAGS_$(my_32_64_bit_suffix)) \
+             $(bc_cflags_opt)
+
 
 ifeq ($(rs_debug_runtime),1)
     bc_cflags += -DRS_DEBUG_RUNTIME
@@ -70,25 +110,25 @@ $(c_bc_files): PRIVATE_INCLUDES := \
     external/clang/lib/Headers
 $(c_bc_files): PRIVATE_CFLAGS := $(bc_cflags)
 
-$(c_bc_files): $(intermediates)/%.bc: $(LOCAL_PATH)/%.c  $(CLANG)
+$(c_bc_files): $(intermediates)/%.bc: $(LOCAL_PATH)/%.c  $(LOCAL_CLANG_EXECUTABLE)
 	@echo "bc: $(PRIVATE_MODULE) <= $<"
 	@mkdir -p $(dir $@)
-	$(hide) $(CLANG) $(addprefix -I, $(PRIVATE_INCLUDES)) $(PRIVATE_CFLAGS) $< -o $@
+	$(hide) $(LOCAL_CLANG_EXECUTABLE) $(addprefix -I, $(PRIVATE_INCLUDES)) $(PRIVATE_CFLAGS) $< -o $@
 
-$(ll_bc_files): $(intermediates)/%.bc: $(LOCAL_PATH)/%.ll $(LLVM_AS)
+$(ll_bc_files): $(intermediates)/%.bc: $(LOCAL_PATH)/%.ll $(LOCAL_LLVM_AS_EXECUTABLE)
 	@mkdir -p $(dir $@)
-	$(hide) $(LLVM_AS) $< -o $@
+	$(hide) $(LOCAL_LLVM_AS_EXECUTABLE) $< -o $@
 
 -include $(c_bc_files:%.bc=%.d)
 -include $(ll_bc_files:%.bc=%.d)
 
 $(LOCAL_BUILT_MODULE): PRIVATE_BC_FILES := $(c_bc_files) $(ll_bc_files)
 $(LOCAL_BUILT_MODULE): $(c_bc_files) $(ll_bc_files)
-$(LOCAL_BUILT_MODULE): $(LLVM_LINK) $(clcore_LLVM_LD)
-$(LOCAL_BUILT_MODULE): $(LLVM_AS) $(BCC_STRIP_ATTR)
+$(LOCAL_BUILT_MODULE): $(LOCAL_LLVM_LINK_EXECUTABLE) $(clcore_LLVM_LD)
+$(LOCAL_BUILT_MODULE): $(LOCAL_LLVM_AS_EXECUTABLE) $(BCC_STRIP_ATTR)
 	@echo "bc lib: $(PRIVATE_MODULE) ($@)"
 	@mkdir -p $(dir $@)
-	$(hide) $(LLVM_LINK) $(PRIVATE_BC_FILES) -o $@.unstripped
+	$(hide) $(LOCAL_LLVM_LINK_EXECUTABLE) $(PRIVATE_BC_FILES) -o $@.unstripped
 	$(hide) $(BCC_STRIP_ATTR) -o $@ $@.unstripped
 
 BCC_RS_TRIPLE :=
--- driver/runtime/ll32/allocation.ll
+++ driver/runtime/ll32/allocation.ll
@@ -1,4 +1,4 @@
-target datalayout = "e-p:32:32:32-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:64:64-f32:32:32-f64:64:64-v64:64:64-v128:64:128-a0:0:64-n32-S64"
+target datalayout = "e-p:32:32:32-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:64:64-f32:32:32-f64:64:64-v64:64:64-v128:64:128-a0:0:64-n8:16:32-S64"
 target triple = "armv7-none-linux-gnueabi"
 
 declare i8* @rsOffset([1 x i32] %a.coerce, i32 %sizeOf, i32 %x, i32 %y, i32 %z)
